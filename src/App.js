a773105946c015131b4c711e3d86cfdc93f63fbf
RitaOak <oak.rita@hotmail.com>
2021-08-18 00:29:23 +0100

    Main project branch. Includes key dependencies such as bulma and sass

--- src/App.js
+++ src/App.js
@@ -1,24 +1,85 @@
-import logo from './logo.svg';
-import './App.css';
+/* eslint-disable jsx-a11y/anchor-is-valid */
+import "./App.css";
+import "./styles/global.scss";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <section class="hero is-medium background">
+        <div class="hero-head">
+          <nav class="navbar">
+            <div class="container">
+              <div class="navbar-brand">
+                <a class="navbar-item">
+                  <img
+                    src="https://bulma.io/images/bulma-type-white.png"
+                    alt="Logo"
+                  ></img>
+                </a>
+                <span class="navbar-burger" data-target="navbarMenuHeroA">
+                  <span></span>
+                  <span></span>
+                  <span></span>
+                </span>
+              </div>
+              <div id="navbarMenuHeroA" class="navbar-menu">
+                <div class="navbar-end">
+                  <a class="navbar-item is-active">case studies</a>
+                  <a class="navbar-item">about me</a>
+                  <span class="navbar-item">
+                    <a class="button is-primary is-inverted">
+                      <span class="icon">
+                        <i class="fab fa-github"></i>
+                      </span>
+                      <span>let's talk</span>
+                    </a>
+                  </span>
+                </div>
+              </div>
+            </div>
+          </nav>
+        </div>
+
+        <div class="hero-body">
+          <div class="container">
+            <p class="title title-styles">
+              <span class="title-name-styles">:</span>
+              <span class="title-name-styles-underlined">rita</span>
+              <span class="title-name-styles">:</span> product designer and
+              frontend developer
+            </p>
+            <p class="subtitle">Subtitle</p>
+          </div>
+        </div>
+
+        <div class="hero-foot">
+          <nav class="tabs">
+            <div class="container">
+              <ul>
+                <li class="is-active">
+                  <a>Overview</a>
+                </li>
+                <li>
+                  <a>Modifiers</a>
+                </li>
+                <li>
+                  <a>Grid</a>
+                </li>
+                <li>
+                  <a>Elements</a>
+                </li>
+                <li>
+                  <a>Components</a>
+                </li>
+                <li>
+                  <a>Layout</a>
+                </li>
+              </ul>
+            </div>
+          </nav>
+        </div>
+      </section>
+    </>
   );
 }
 
