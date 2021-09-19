cef27d29a61310860ca52f8b83ccccc564a950c8
RitaOak <oak.rita@hotmail.com>
2021-09-19 17:35:47 +0100

    fix: improved landing page intro text, still needs icons and some adjustments

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
 import Navbar from "./components/navigation/navbar";
-import SocialMedia from "./components/header/socialMedia";
+//import SocialMedia from "./components/header/socialMedia";
 import Header from "./components/header/header";
 import { Hero } from "react-bulma-components";
 import { Container } from "react-bulma-components";
@@ -18,8 +18,6 @@ function App() {
           <Header />
         </Container>
       </Hero.Body>
-      {/* Footer */}
-      <SocialMedia />
     </Hero>
   );
 }
--- src/components/header/header.js
+++ src/components/header/header.js
@@ -2,6 +2,10 @@ import React from "react";
 import "../header/header.scss";
 import { BsArrowReturnRight } from "react-icons/bs";
 import { MdOpenInNew } from "react-icons/md";
+import { FaBehance } from "react-icons/fa";
+import { FaDribbbleSquare } from "react-icons/fa";
+import { FaGithub } from "react-icons/fa";
+import { FaLinkedin } from "react-icons/fa";
 import DownloadCV from "./downloadCV.js";
 import SocialMedia from "./socialMedia.js";
 
@@ -9,16 +13,42 @@ const header = () => {
   return (
     <div>
       <p class="title title-styles">
-        <span class="title-name-styles">:</span>
-        <span class="title-name-styles-underlined">rita</span>
-        <span class="title-name-styles">:</span> product designer and junior
-        frontend developer
+        Hello there! I'm <span class="title-name-styles-underlined">Rita</span>{" "}
+        -- a product designer and junior frontend developer living in Portugal.
+        You can find my projects on{" "}
+        <a
+          href="https://dribbble.com/OakRita"
+          rel="noopener noreferrer"
+          target="_blank"
+        >
+          <span>
+            <FaDribbbleSquare
+              size={50}
+              style={{ paddingTop: 5, color: "#F8FFAE" }}
+            />
+          </span>
+        </a>{" "}
+        or{" "}
+        <a
+          href="https://www.behance.net/ritaoak"
+          rel="noopener noreferrer"
+          target="_blank"
+        >
+          <span>
+            <FaBehance
+              size={50}
+              size={50}
+              style={{ paddingTop: 5, color: "#50E179" }}
+            />
+          </span>
+        </a>
+        .
       </p>
       <p class="subtitle subtitle-format">
         <span>
           <BsArrowReturnRight />{" "}
         </span>
-        Currently working as a contractor to redesign and implement the{" "}
+        I'm currently working as a contractor to redesign and implement the{" "}
         <a
           href="https://www.apereo.org/projects/apereo-oae"
           rel="noopener noreferrer"
